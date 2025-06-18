9a04f83a698d0ce8e2fee5d29fbefa7df63662b6
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-18 07:21:47 +0100

    Update dashboard_financial.py

--- dashboard_financial.py
+++ dashboard_financial.py
@@ -9,7 +9,7 @@ from ratio import clean_number, calculate_ratios, select_key_for_year
 from predict import predict_labels
 
 
-st.set_page_config(page_title="Financial Dashboard", layout="centered")
+#st.set_page_config(page_title="Financial Dashboard", layout="centered")
 # Directory containing images
 OUTPUT_DIR = "output_images"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
